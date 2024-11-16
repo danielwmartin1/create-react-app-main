c5cca985f72360c1c59feb3fb41e46bb01eb8d58
danielwmartin1 <danielwmartin1@gmail.com>
2024-11-16 03:17:06 -0500

    Refactor Projects component for improved styling; update link colors and structure, and remove unnecessary fragments

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,18 +3,17 @@ import "../App.css";
 
 const Projects = () => {
     return (
-        <>
-            <h1>Projects</h1>
-            <p>This is the Projects component.</p>
-            <a href="https://github.com/danielwmartin1?tab=repositories" target="_blank" rel="noopener noreferrer">
+        <div className='container main' style={{ color: 'white' }}>
+            <h1 style={{color: 'aqua'}}>Projects</h1>
+            <a href="https://github.com/danielwmartin1?tab=repositories" target="_blank" rel="noopener noreferrer" style={{ color: 'aqua' }}>
                 Visit my GitHub repository
             </a>
-            <ul>
-                <li><a href="https://tic-tac-toe-kohl-iota.vercel.app/" target="_blank" rel="noopener noreferrer">Project 1</a></li>
-                <li><a href="https://todolist-frontend-tau.vercel.app/" target="_blank" rel="noopener noreferrer">Project 2</a></li>
-                <li><a href="https://todolist-backend-six-woad.vercel.app/tasks/" target="_blank" rel="noopener noreferrer">Project 3</a></li>
+            <ul style={{ listStyleType: 'none' }}>
+                <li><a href="https://tic-tac-toe-kohl-iota.vercel.app/" target="_blank" rel="noopener noreferrer" style={{ color: 'white' }}>Project 1</a></li>
+                <li><a href="https://todolist-frontend-tau.vercel.app/" target="_blank" rel="noopener noreferrer" style={{ color: 'white' }}>Project 2</a></li>
+                <li><a href="https://todolist-backend-six-woad.vercel.app/tasks/" target="_blank" rel="noopener noreferrer" style={{ color: 'white' }}>Project 3</a></li>
             </ul>
-        </>
+        </div>
     );
 };
 
