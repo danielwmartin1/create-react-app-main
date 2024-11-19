439d03a9b7ab19cee8cf7741fc0e4dae3775b5e0
danielwmartin1 <danielwmartin1@gmail.com>
2024-11-19 16:27:46 -0500

    Add project class to style project links and enhance layout in Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,13 +5,13 @@ const Projects = () => {
     return (
         <div className='container main' style={{marginTop: '5rem', border: '3px solid aqua'}}>
             <h1 style={{margin: '2rem', color: 'white'}} >Projects</h1>
-            <a style={{margin: '2rem'}} className='anchor' href="https://github.com/danielwmartin1?tab=repositories" target="_blank" rel="noopener noreferrer">
+            <a style={{margin: '2rem'}} className='anchor project' href="https://github.com/danielwmartin1?tab=repositories" target="_blank" rel="noopener noreferrer">
                 Visit my GitHub repository
             </a>
             <ul style={{ listStyleType: 'none', margin: '2rem' }}>
-                <li><a className='anchor' href="https://tic-tac-toe-kohl-iota.vercel.app/" target="_blank" rel="noopener noreferrer">Project 1</a></li>
-                <li><a className='anchor' href="https://todolist-frontend-tau.vercel.app/" target="_blank" rel="noopener noreferrer" >Project 2</a></li>
-                <li><a className='anchor' href="https://todolist-backend-six-woad.vercel.app/tasks/" target="_blank" rel="noopener noreferrer">Project 3</a></li>
+                <li><a className='anchor project' href="https://tic-tac-toe-kohl-iota.vercel.app/" target="_blank" rel="noopener noreferrer">Project 1</a></li>
+                <li><a className='anchor project' href="https://todolist-frontend-tau.vercel.app/" target="_blank" rel="noopener noreferrer" >Project 2</a></li>
+                <li><a className='anchor project' href="https://todolist-backend-six-woad.vercel.app/tasks/" target="_blank" rel="noopener noreferrer">Project 3</a></li>
             </ul>
         </div>
     );
