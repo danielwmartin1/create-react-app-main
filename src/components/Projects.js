828181da4c218ac895d3218b57a404bf93476980
danielwmartin1 <danielwmartin1@gmail.com>
2024-11-22 00:39:24 -0500

    Update Projects component: change link text and adjust styling for consistency

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,9 +6,9 @@ const Projects = () => {
         <div className='container main' style={{ border: '3px solid aqua', marginTop: '2rem'}}>
             <h1 style={{margin: '2rem', color: 'white'}} >Portfolio</h1>
             <a style={{margin: '2rem'}} className='anchor project' href="https://github.com/danielwmartin1?tab=repositories" target="_blank" rel="noopener noreferrer">
-                Visit my GitHub Repositories
+                Check out my GitHub Repositories Page
             </a>
-            <ul style={{ listStyleType: 'none', margin: '2rem' }}>
+            <ul style={{ listStyleType: 'none' }}>
                 <li><a className='anchor project' href="https://tic-tac-toe-kohl-iota.vercel.app/" target="_blank" rel="noopener noreferrer">Project 1</a></li>
                 <li><a className='anchor project' href="https://todolist-frontend-tau.vercel.app/" target="_blank" rel="noopener noreferrer" >Project 2</a></li>
                 <li><a className='anchor project' href="https://todolist-backend-six-woad.vercel.app/tasks/" target="_blank" rel="noopener noreferrer">Project 3</a></li>
