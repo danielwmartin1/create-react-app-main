715dd1c64e661fcd5def17b01d766ff11a15873e
danielwmartin1 <danielwmartin1@gmail.com>
2024-11-27 01:28:07 -0500

    Refactor Main.js to improve structure by grouping related elements into separate containers for better readability and organization.

--- src/components/Main.js
+++ src/components/Main.js
@@ -48,12 +48,16 @@ const logos = [
 
 const Main = () => (
   <div className="container main">
-    <img id="profile" src={profileImg} alt="Profile" />
-    <h1 id="name" className='headings'>Daniel Martin</h1>
-    <p className="p">Hello! I'm Daniel, a passionate web developer with experience in creating dynamic and responsive websites. I love coding and enjoy learning new technologies. In my free time, I like to work on personal projects and contribute to open-source communities.</p>
-    <h2 id="experience" className='headings'>Experience</h2>
-    <p className="p">With hands-on experience in web development, I have honed my skills in HTML, CSS, JavaScript, and various modern frameworks like React. My journey has been enriched by working on diverse projects that range from small business websites to large-scale web applications. This practical experience has provided me with a deep understanding of web technologies.</p>
-    <div className="container logo-container">
+    <div className="container">
+      <img id="profile" src={profileImg} alt="Profile" />
+      <h1 id="name" className='headings'>Daniel Martin</h1>
+      <p className="p">Hello! I'm Daniel, a passionate web developer with experience in creating dynamic and responsive websites. I love coding and enjoy learning new technologies. In my free time, I like to work on personal projects and contribute to open-source communities.</p>
+    </div>
+    <div className="container">
+      <h2 id="experience" className='headings'>Experience</h2>
+      <p className="p">With hands-on experience in web development, I have honed my skills in HTML, CSS, JavaScript, and various modern frameworks like React. My journey has been enriched by working on diverse projects that range from small business websites to large-scale web applications. This practical experience has provided me with a deep understanding of web technologies.</p>
+      </div>
+    <div className="container">
       <h2 id="tech" className='headings'>Technologies</h2>
       <div className="logos">
         {logos.map((logo, index) => (
