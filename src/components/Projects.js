442f77769023cbce84ba679e712f6a4aee992444
danielwmartin1 <danielwmartin1@gmail.com>
2024-12-03 00:01:01 -0500

    Reorder project links for better organization; enhance GitHub link styling with padding for improved visibility

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,15 +5,15 @@ const Projects = () => {
     const projectLinks = [
         { href: "https://danielwmartin1.github.io/first-website/", text: "My First Website" },
         { href: "https://tic-tac-toe-kohl-iota.vercel.app/", text: "Tic-Tac-Toe" },
-        { href: "https://todolist-frontend-tau.vercel.app/", text: "To-Do List" },
-        { href: "https://weather-app-danielwmartin1s-projects.vercel.app/", text: "Weather App" }
+        { href: "https://weather-app-danielwmartin1s-projects.vercel.app/", text: "Weather App" },
+        { href: "https://todolist-frontend-tau.vercel.app/", text: "To-Do List" }
     ];
 
     return (
         <div className='container main project-div'>
             <h1>Portfolio</h1>
             <div className="project-link anchor project divProject">
-                <a className='anchor project' style={{textDecoration: 'underline'}} href="https://github.com/danielwmartin1?tab=repositories" target="_blank" rel="noopener noreferrer">
+                <a className='anchor project' style={{textDecoration: 'underline', paddingBottom: '1rem'}} href="https://github.com/danielwmartin1?tab=repositories" target="_blank" rel="noopener noreferrer">
                     My GitHub Repositories
                 </a>
                 <div className='project-link'>
