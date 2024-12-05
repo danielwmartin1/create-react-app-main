6dbee78a156cebaab77531b0c9ae7703d5228238
danielwmartin1 <danielwmartin1@gmail.com>
2024-12-05 02:07:24 -0500

    Add portfolio styling and update header class for improved layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ const Projects = () => {
 
     return (
         <div className='container main project-div'>
-            <h1>Portfolio</h1>
+            <h1 className="portfolio">Portfolio</h1>
             <div className="project-link anchor project divProject">
                 <a className='anchor project' style={{textDecoration: 'underline', paddingBottom: '1rem'}} href="https://github.com/danielwmartin1?tab=repositories" target="_blank" rel="noopener noreferrer">
                     My GitHub Repositories
