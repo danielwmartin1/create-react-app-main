f4261e70913e4dafc0f2a69913062b889ebb67c8
danielwmartin1 <danielwmartin1@gmail.com>
2024-12-05 03:50:28 -0500

    Refactor layout in App.css and Projects.js for improved structure and design consistency

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,19 +13,21 @@ const Projects = () => {
 
     return (
         <div className='container main project-div'>
-            <h1 className="portfolio">Portfolio</h1>
-            <div className="project-link anchor project divProject">
-                <a className='anchor project' style={{textDecoration: 'underline', paddingBottom: '1rem'}} href="https://github.com/danielwmartin1?tab=repositories" target="_blank" rel="noopener noreferrer">
-                    My GitHub Repositories
-                </a>
-                <div className='project-link'>
-                    {projectLinks.map((project, index) => (
-                        <div>
-                            <a key={index} className='anchor project' href={project.href} target="_blank" rel="noopener noreferrer">
-                                {project.text}
-                            </a>
-                        </div>
-                    ))}
+            <div className="portfolio-container">
+                <h1 className="portfolio">Portfolio</h1>
+                <div className="project-link anchor project divProject">
+                    <a className='anchor project' style={{textDecoration: 'underline', paddingBottom: '1rem'}} href="https://github.com/danielwmartin1?tab=repositories" target="_blank" rel="noopener noreferrer">
+                        My GitHub Repositories
+                    </a>
+                    <div className='project-link'>
+                        {projectLinks.map((project, index) => (
+                            <div>
+                                <a key={index} className='anchor project' href={project.href} target="_blank" rel="noopener noreferrer">
+                                    {project.text}
+                                </a>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
